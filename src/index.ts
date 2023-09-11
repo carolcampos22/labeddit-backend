396bb2380624405e64604bf4b0d4e830f3491b03
carolcampos22 <mendes.carolina2209@gmail.com>
2023-09-11 17:10:27 -0300

    Estruturação do projeto

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT || 3003, () => {
+    console.log(`Servidor rodando na porta ${3003}`)
+})
+
+/**exemplos:
+ * app.use("/users", userRouter)
+
+app.use("/posts", postRouter)
+ */
